bd8fcbaa13ddae78a44bfcf51972f9429f552637
Ding, Zuosheng <Zuosheng.Ding@adesso.de>
2023-04-06 11:40:56 +0200

    add all codes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { PostslistComponent } from './posts/postslist/postslist.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
+    path: 'counter',
+    component:CounterComponent
+  },
+  {
+    path: 'posts',
+    component:PostslistComponent,
+    children:[
+      {
+        path: 'add',
+        component:AddPostComponent
+      }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
